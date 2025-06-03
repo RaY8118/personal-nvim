cf5dec201c816018e56a95b2027aecfc95b149de
RaY8118 <parthghadi43@gmail.com>
2025-06-03 15:11:27 +0000

    Add docker, json, markdown LSP configs

--- lua/kickstart/plugins/lsp-config.lua
+++ lua/kickstart/plugins/lsp-config.lua
@@ -75,12 +75,6 @@ return {
       gopls = {},
       -- pyright = {},
       -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
       ts_ls = {},
       tailwindcss = {
         filetypes_exclude = { 'markdown' },
@@ -113,6 +107,23 @@ return {
         },
       },
       jdtls = {},
+      dockerls = {},
+      docker_compose_language_service = {},
+      marksman = {},
+      jsonls = {
+        on_new_config = function(new_config)
+          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+          vim.list_extend(new_config.settings.json.schemas, require('schemastore').json.schemas())
+        end,
+        settings = {
+          json = {
+            format = {
+              enable = true,
+            },
+            validate = { enable = true },
+          },
+        },
+      },
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
@@ -134,6 +145,7 @@ return {
     vim.list_extend(ensure_installed, {
       'stylua',
       'codelldb',
+      'hadolint',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
     require('mason-lspconfig').setup {
